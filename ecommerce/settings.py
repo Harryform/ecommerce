40d25ccc9b57526063ce9ae0fec28c37ac2f47f1
Harrison Bush <harryform@Harrisons-MacBook-Pro.local>
2022-03-10 04:46:21 -0500

    adjusted settings file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o!i5k30uq9+zb6-6_xn&b%8c!(6-sif0s#wcc167c!(s%lub@_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoe-stopper.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
